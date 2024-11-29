b62c5e2db05a2ad0a9d36967af97e65f38544417
Offermann Hannah Pernilla <hannah.offermann@edu.fh-joanneum.at>
2024-11-29 17:12:13 +0100

    simple hello world for setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 4000;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/html/index.html"));
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}`)
+})
+
