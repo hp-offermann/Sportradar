c6f9b38b9215219c4483ab98111b861c599111e8
Offermann Hannah Pernilla <hannah.offermann@edu.fh-joanneum.at>
2024-11-30 00:29:02 +0100

    fetching JSON data works and moved JSON file

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
+function fetchJSON () {
+    fetch("./sportData.json").then((r) => {
+        if (!r.ok) {
+            throw new Error(`${r.status}`);
+        }
+        return r.json();
+    }).then((data) => console.log(data)).catch((error) => console.log("couldnt fetch data", error));
+}
+fetchJSON();
+
+
 const EventPage = document.getElementById('btn_Event');
 
 if(EventPage){
