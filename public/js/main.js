5bc153281d96e8e95f3f65b79a069d8b3d9454b2
Offermann Hannah Pernilla <hannah.offermann@edu.fh-joanneum.at>
2024-11-30 02:02:20 +0100

    marker appears on date with event, but amount of dots doesnt equal amounts of events --> important FIX!!

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,29 @@ function fetchJSON () {
             throw new Error(`${r.status}`);
         }
         return r.json();
-    }).then((data) => console.log(data)).catch((error) => console.log("couldnt fetch data", error));
+    }) .then((data) => {
+        console.log(data);
+
+        const sportData = data.data;
+
+        sportData.forEach(event => {
+            const calendar = document.querySelectorAll(".marker");
+            const date = event.dateVenue;
+
+            calendar.forEach(day => {
+                if (day.id === date) {
+                    const button = document.createElement("entry-button");
+                    button.classList.add("viewEntry");
+
+                    day.appendChild(button);
+                }
+            });
+        });
+
+    }).catch((error) =>
+        console.log("could not fetch data", error));
 }
 fetchJSON();
-
 
 const EventPage = document.getElementById('btn_Event');
 
