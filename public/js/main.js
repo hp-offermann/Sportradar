c327c052242b7db7d7adbc5c09dfcce3f28c7737
Offermann Hannah Pernilla <hannah.offermann@edu.fh-joanneum.at>
2024-11-30 10:34:03 +0100

    added ids to form and get formdata on submit

--- public/js/main.js
+++ public/js/main.js
@@ -12,11 +12,11 @@ function fetchJSON () {
 
         sportData.forEach(event => {
             const date = event.dateVenue;
-            console.log(`Eventdate: ${date}`);
+            //console.log(`Eventdate: ${date}`);
 
             calendar.forEach(day => {
                 if (day.id === date) {
-                    console.log(`Marker added for: ${date}`);
+                    //console.log(`Marker added for: ${date}`);
                     const button = document.createElement("entry-button");
                     button.classList.add("viewEntry");
 
@@ -44,6 +44,26 @@ if(HomePage){
         window.location.href = `/`;
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("addEvent");
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const formData = new FormData(form);
+        const eventData = {};
+
+        for (let [key, value] of formData.entries()) {
+            eventData[key] = value;
+        }
+
+        console.log('Object:', eventData);
+        form.reset();
+    });
+
+})
+
+
 
 
 
