ceeba12f5c8f4251b807208b6232285659e2b4e6
Offermann Hannah Pernilla <hannah.offermann@edu.fh-joanneum.at>
2024-12-01 14:56:44 +0100

    starting with calendar render function and small changes in html

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,43 @@
 const form = document.getElementById("addEvent");
 let allData = JSON.parse(localStorage.getItem('sportData')) || [];
+
+
 const EventPage = document.getElementById('btn_Event');
 const HomePage = document.getElementById('btn_Home');
-const calendar = document.querySelector(".calendar");
 
+const calendar = document.querySelector(".calendar");
+const days = document.getElementById("days");
+
+let date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+//console.log(date, year, month);
+
+function renderCalendar(){
+    let datesLastMonth = new Date(year, month, 0).getDate();
+    let firstDayMonth = new Date(year, month, 1).getDay();
+    let lastDayMonth = new Date(year, month +1,0).getDate();
+    //console.log(lastDayMonth);
+
+    let div = "";
+
+    for (let i=firstDayMonth; i > 0; i--){
+        div += `<div class="previous-month">${datesLastMonth -i +1}</div>`;
+    }
+
+    for (let i = 1; i <= lastDayMonth; i++) {
+        div += `<div class="current-month">${i}</div>`;
+    }
+
+    days.innerHTML = div;
+
+    console.log(firstDayMonth);
+    console.log(lastDayMonth);
+
+}
+renderCalendar();
+
+/*
 function fetchJSON () {
     fetch("./sportData.json").then((r) => {
         if (!r.ok) {
@@ -80,6 +114,8 @@ if(form){
         form.reset();
     });
 }
+
+ */
 
 if(calendar){
     calendar.addEventListener('click', function(event) {
